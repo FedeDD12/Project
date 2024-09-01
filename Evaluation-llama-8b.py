a5f9121d09c7f7bcdb75c0e9114456f8386d48e5
FedeDD12 <federica.delduchetto@studio.unibo.it>
2024-09-01 09:12:44 +0200

    Update Evaluation-llama-8b.py

--- Evaluation-llama-8b.py
+++ Evaluation-llama-8b.py
@@ -1,11 +1,17 @@
-
+import transformers
 import pandas as pd
 import torch
 from transformers import AutoTokenizer
 
+from huggingface_hub.hf_api import HfFolder; HfFolder.save_token("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")
 
-
+model_name="meta-llama/Meta-Llama-3.1-8B"
+tokenizer=AutoTokenizer(model_name)
 eval_dataset=pd.read_csv("eval_dataset.csv")
+
+for question in eval_dataset["Question"]:
+  input_text=f"### Question: {question} ### Answer:"
+  
 
 
 
