932a488dd0ff085a6aa55647f0bc50757b9bbf9d
FedeDD12 <federica.delduchetto@studio.unibo.it>
2024-09-01 09:07:43 +0200

    Update Evaluation-llama-8b.py

--- Evaluation-llama-8b.py
+++ Evaluation-llama-8b.py
@@ -3,10 +3,9 @@ import pandas as pd
 import torch
 import ollama
 
-from huggingface_hub.hf_api import HfFolder; HfFolder.save_token("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")
-
 eval_dataset=pd.read_csv("eval_dataset.csv")
 
+qa_pairs=[]
 for question in eval_dataset["Question"]:
     input_text=f"### Question: {question} ### Answer:"
     response = ollama.chat(model='llama3.1:8b-instruct-q8_0', messages=[
@@ -19,39 +18,17 @@ for question in eval_dataset["Question"]:
     qa_text = response['message']['content']
 
     try:
-        question, answer = qa_text.split('**Answer:**')
+        question, answer = qa_text.split('### Answer:')
+        question=question.replace("## Question:", "")
+    except Exception as e:
+        print("Error {} encountered...skipping...".format(e))
+        continue
+    
 
-for i, text in enumerate(dataset["text"].values()):
-  # print(text)
-  # print('Generate me a question and an answer about the content of the following article "{}" in the following format:\
-      # Question:\
-      # Answer:'.format(text))
-  response = ollama.chat(model='llama3.1:8b-instruct-q8_0', messages=[
-    {
-      'role': 'user',
-      'content': 'Generate me only one question and answer pair about the content of the following article "{}".\
-      The response must be in the following format:\
-      "**Question:** <question text>\
-      **Answer:** <answer text>"\
-      Do not add an additional newline between the question and the answer.\
-      Do not add any additional text other than the question and answer, do not offer other information.\
-      The question must not be about mathematicians\' life.\
-      If the article is about a mathematicians\' life do not generate anything.\
-      The question must be a mathematical fact.\
-      The answer must be as complete as possible and correlated by an explaination.\
-      Do not give questions and answers about books. If the article is only about a book do not write anything.\
-      Do not give answers that are too simple.\
-      In the answer do not add any newline.\
-      Write the answer in only one line.\
-      Do not generate any list. \
-      Assume that the person reading the question and answer has no access to the article and has no prior knowledge of the topic. Provide the necessary context.\
-      '.format(text),
-    },
-  ])
+    qa_pairs.append((question.strip(), answer.strip()))
 
-  print(i, response)
-  # Extract the question and answer from the response
-  qa_text = response['message']['content']
+    # Write the question-answer pairs to a CSV file
+    with open('questions_and_answers.csv', 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(qa_pairs[-1]) 
 
-  try:
-    question, answer = qa_text.split('**Answer:**')
