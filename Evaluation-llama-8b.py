1d7c44944b5c753782a9c23f6c0be6a528dba8b2
FedeDD12 <federica.delduchetto@studio.unibo.it>
2024-09-01 09:19:48 +0200

    Update Evaluation-llama-8b.py

--- Evaluation-llama-8b.py
+++ Evaluation-llama-8b.py
@@ -13,8 +13,24 @@ tokenizer.pad_token = tokenizer.eos_token
 eval_dataset=pd.read_csv("eval_dataset.csv")
 eval_dataset=Dataset.from_pandas(eval_dataset)
 
+results=[]
 for question in eval_dataset["Question"]:
   input_text=f"### Question: {question} ### Answer:"
+  inputs=tokenizer(input_text, return_tensors="pt")
+  outputs=model.generate(**inputs, max_length=256)
+
+  generated_text= tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+  if "### Answer:" in generated_text:
+    answer = generated_text.split("### Answer:")[1].strip()
+  else:
+    answer = generated_text.strip()
+    
+  results.append({'Question': question, 'Generated Text': answer})
+  print(f"Generated text: {generated_text}")
+
+df = pd.DataFrame(results)
+df.to_csv('results_llama-8b.csv', index=False)
   
   
 
