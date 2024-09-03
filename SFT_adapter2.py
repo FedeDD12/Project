e36eb3aea7ad47b4c4249ad8d96af3f647a4247b
FedeDD12 <federica.delduchetto@studio.unibo.it>
2024-09-03 19:06:31 +0200

    Update SFT_adapter2.py

--- SFT_adapter2.py
+++ SFT_adapter2.py
@@ -127,4 +127,4 @@ for question in eval_dataset["Question"]:
     print(f"Generated text: {generated_text}")
 
 df = pd.DataFrame(results)
-df.to_csv('results.csv', index=False)
+df.to_csv('results2.csv', index=False)
