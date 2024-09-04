27e64de050686903e380a78a786da1a22240e0b2
FedeDD12 <federica.delduchetto@studio.unibo.it>
2024-09-04 16:36:24 +0200

    Update Evaluation-ollama.py

--- Evaluation-ollama.py
+++ Evaluation-ollama.py
@@ -4,11 +4,11 @@ import csv
 
 dataset=pd.read_csv("eval_dataset.csv")
 
-qa_pairs=[]
+results=[]
 
 with open('results_noSFT.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(['Question', 'Answer'])  # Write the header row
+    writer.writerow(['Question', 'Generated Text'])  # Write the header row
 
 
 for question in dataset["Question"]:
@@ -24,15 +24,15 @@ for question in dataset["Question"]:
   # Extract the question and answer from the response
   qa_text = response['message']['content']
 
-  try:
-    question, answer = qa_text.split('### Answer:')
+  #try:
+    #question, answer = qa_text.split('### Answer:')
 
-    question=question.replace("### Question:", "")
-  except Exception as e:
-    print("Error {} encountered...skipping...".format(e))
-    continue
+    #question=question.replace("### Question:", "")
+  #except Exception as e:
+    #print("Error {} encountered...skipping...".format(e))
+    #continue
 
-  qa_pairs.append((question.strip(), answer.strip()))
+  results.append((question.strip(), qa_text.strip())
 
   with open('results_noSFT.csv', 'a', newline='', encoding='utf-8') as file:
       writer = csv.writer(file)
